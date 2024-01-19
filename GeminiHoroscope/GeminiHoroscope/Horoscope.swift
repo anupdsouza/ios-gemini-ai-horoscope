5c7d6deac57a4854c74b8e9a81bdbdb6e161661c
Anup DSouza <anup.dsouza@gmail.com>
2024-01-19 15:14:35 +0530

    Remove unused models

--- GeminiHoroscope/GeminiHoroscope/Horoscope.swift
+++ GeminiHoroscope/GeminiHoroscope/Horoscope.swift
@@ -13,17 +13,8 @@ struct Horoscope: Codable {
     let horoscope_data: String
 }
 
-enum HoroscopeError: Error {
-    case invalidData
-}
-
 struct HoroscopeResponse: Decodable {
     let data: Horoscope
     let status: Int
     let success: Bool
 }
-//
-//struct HoroscopeData: Codable {
-//    let date: String
-//    let horoscope_data: String
-//}
